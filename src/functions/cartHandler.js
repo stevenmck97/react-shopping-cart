4a51be6738e61a2a7de4e7dbe2ab7b5a09291b2c
Steven McKeown <stevenmckeown19@gmail.com>
2022-06-02 21:54:03 +0100

    Refactored addToCartHandler to use anonymous function

--- src/functions/cartHandler.js
+++ src/functions/cartHandler.js
@@ -1,25 +1,22 @@
-export const addToCartHandler = (cartItem, setCartItem, games) => {
-    const addCart = (e) => {
-        e.preventDefault();
-        const itemName = e.target.name;
-        const storeItem = games.find((obj) => obj.name === itemName);
-        const stateItem = cartItem.find((obj) => obj.name === itemName);
+export const addToCartHandler = (cartItem, setCartItem, games) => (e) => {
+    e.preventDefault();
+    const itemName = e.target.name;
+    const storeItem = games.find((obj) => obj.name === itemName);
+    const stateItem = cartItem.find((obj) => obj.name === itemName);
 
-        /* Checks if the item is in the cart. If it isn't, it adds
-       it to the cart. If it is, it adds one to the count*/
-        !stateItem
-            ? setCartItem(cartItem.concat(storeItem))
-            : setCartItem(
-                  cartItem.map((item) =>
-                      item.name === itemName
-                          ? {
-                                ...item,
-                                count: item.count + 1,
-                                total: item.price * (item.count + 1),
-                            }
-                          : item
-                  )
-              );
-    };
-    return addCart;
+    /* Checks if the item is in the cart. If it isn't, it adds
+   it to the cart. If it is, it adds one to the count*/
+    !stateItem
+        ? setCartItem(cartItem.concat(storeItem))
+        : setCartItem(
+              cartItem.map((item) =>
+                  item.name === itemName
+                      ? {
+                            ...item,
+                            count: item.count + 1,
+                            total: item.price * (item.count + 1),
+                        }
+                      : item
+              )
+          );
 };
